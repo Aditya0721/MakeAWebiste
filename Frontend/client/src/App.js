792d48fa9cb35edac83b534f045997e1439eb2ee
Aditya0721 <adityaprasad246278@gmail.com>
2024-05-13 18:13:14 +0530

    defined routes

--- Frontend/client/src/App.js
+++ Frontend/client/src/App.js
@@ -1,11 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './Screens/HomeApp';
-import React from 'react';
+import React, { useEffect } from 'react';
+import { BrowserRouter, Route, Routes, useNavigate } from 'react-router-dom';
+import GymTemplate from './Templates/Gym/GymTemplate';
+import TransportationTemplate from './Templates/Transportaion/TransportationTemplate';
+import Template from './Templates/Template';
+import Content from './Screens/Content';
 
 function App() {
+
   return (
-    <Home></Home>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}>
+          <Route index element={<Content/>}></Route>
+          <Route path='content' element={<Content/>}/>
+          <Route path='template' element={<Template/>}>
+            <Route path='gym' element={<GymTemplate/>}/>
+            <Route path='transportation' element={<TransportationTemplate/>}/>
+          </Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+
   );
 }
 
